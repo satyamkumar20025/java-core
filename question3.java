af1ab3884208642ea02898d305e90f817b01ee37
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-08-31 14:19:42 +0530

    Create question3.java

---
+++ question3.java
@@ -0,0 +1,20 @@
+package conditional_questions ;
+import java.util.Scanner;
+ public class questions3  {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the check year:");
+		int num = sc.nextInt();
+		if(num % 4 == 0 && num % 100 != 0 || num % 400 == 0)
+		{
+			System.out.println("this is leap year :");
+		}
+		
+		else {
+			System.out.println("this is not leap year :");
+		}; 
+	}
+	
+}
+
