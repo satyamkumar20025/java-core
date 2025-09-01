84156d00f71a52a7428daf2d7e91b6a5c9253eb4
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-09-01 20:42:21 +0530

    Create pattern8.java

---
+++ pattern8.java
@@ -0,0 +1,43 @@
+package conditional_questions;
+
+import java.util.Scanner;
+
+public class pattern20 {
+	public static void main(String[] args)
+	{
+		 Scanner sc = new Scanner(System.in);
+				System.out.print("Enter the any hight of hourglass :");
+		         int n1 = sc.nextInt();
+		     
+		          for(int i=n1;i>=1;i--)
+		          {
+		        	  for(int k=1;k<=n1-i;k++)
+		        	  {
+		        		  System.out.print(" ");
+		        		  
+		        	  }
+		        	  for(int j=1;j<=2*i-1;j++)
+		        	  {
+		        		  System.out.print("*");
+		        	  }
+		        	  System.out.println();
+		          }
+		         for( int i=1;i<=n1-1; i++)
+		             {
+		        	  for(int k = 1; k<n1-i;k++)
+		        	   {
+		        	     System.out.print(" ");
+		        	    }
+		        	  for(int j=-1;j<=2*i-1;j++)
+		        	   {
+		        	    System.out.print("*");
+		        	  	 }
+		        	 System.out.println();
+		             }
+		         
+	}
+		        	  
+	};
+
+
+
