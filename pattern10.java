3d599182662f0b4e806402dc70ff36bbc4acf94a
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-09-01 20:46:16 +0530

    Create pattern10.java

---
+++ pattern10.java
@@ -0,0 +1,32 @@
+package conditional_questions;
+
+  import java.util.Scanner;
+
+public class pattern17 {
+	public static void main(String[] args)
+	{
+		  Scanner sc = new Scanner(System.in);
+				System.out.print("Enter the any digit :");
+		          int n = sc.nextInt();
+		           int num = 1;
+		        		   
+		          for( int i=1;i <= n; i++)
+		          {
+		        	  
+		        	  for(int j = 1; j<=i;j++)
+		        	  {  
+	        			  System.out.print(i);
+	        			    
+	        			  
+		        	  }
+
+		               System.out.println();
+
+		          }
+		          sc.close();
+		          
+	              
+
+	};
+
+}
