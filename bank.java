c39b5ddea5c42e7041b3c87d815ece25295a1db3
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-09-07 12:39:48 +0530

    Create bank.java

---
+++ bank.java
@@ -0,0 +1,78 @@
+package exception_handling;
+
+import java.util.Scanner;
+
+public class Bank {
+	int acc_no;
+	String name,bra,Enter;
+	float add;
+	float current,curr;
+	float  with,pre;
+	
+
+	 void input()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the accountnumber:");
+		acc_no=sc.nextInt();
+		 sc.nextLine();
+        System.out.print("Enter the name:");
+        name=sc.nextLine();
+        
+        
+        System.out.print("Enter the branch:");
+        bra=sc.nextLine();
+
+        System.out.print("Enter the current balaance");
+        current=sc.nextFloat();
+        
+	}
+       void add_money() {
+   		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the add money :");
+		add=sc.nextFloat();
+		System.out.print("add money successfull:");
+		
+		}
+       void withdraw()
+        {
+    	   Scanner sc = new Scanner (System.in);
+        	System.out.print("Enter the withdraw amount:");
+   		 sc.nextLine();
+
+        	if(current<=with)
+        	{
+            	curr-=with;
+                pre=curr;
+        		System.out.print("withdraw successfull"+pre);
+        	}
+        	else {
+        		System.out.print("not withdraw"+curr);
+        	}
+        	
+        }
+        
+	
+	void display()
+	{
+		System.out.println(" name"+name);
+		System.out.println("account number"+acc_no);
+		System.out.println("present amount"+pre);
+		System.out.println(" withdraw amount"+with);
+		System.out.println(" branch name"+bra);
+		
+		
+
+		}
+	
+		public static void main(String[] args) {
+			Bank s1=new Bank();
+			s1.input();
+			s1.add_money();
+			s1.withdraw();
+			s1.display();
+		}
+}
+
+
