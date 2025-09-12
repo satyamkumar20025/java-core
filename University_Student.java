b6db4f830467249be0f6bd3a088f2f9cb77b340a
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-09-12 13:43:07 +0530

    Update University_Student.java

--- University_Student.java
+++ University_Student.java
@@ -1,12 +1,12 @@
-package exception_handling;
+package Function;
 
 import java.util.Scanner;
 
-public class University_Student {
+public class university_student {
 	String name;
 	private int roll_no;
-	private float marks1,marks2,marks3,marks4,marks5;
-	private float total,avg,per,Gpa;
+	private double marks1,marks2,marks3,marks4,marks5;
+	private double total,avg,per,Gpa;
 
 	 void input()
 	{
@@ -15,7 +15,7 @@ public class University_Student {
 		name=sc.nextLine();
         System.out.print("Enter the roll no name:");
         roll_no=sc.nextInt();
-        System.out.print("Enter the any 3 subject marks:");
+        System.out.print("Enter the any 5 subject marks:");
 		marks1=sc.nextFloat();
 		marks2=sc.nextFloat();
 		marks3=sc.nextFloat();
@@ -41,32 +41,50 @@ public class University_Student {
 			System.out.print("pass");
 		}
 		else {
-			System.out.print("fall");
+			System.out.print("fail");
 		}
 	}
 	void Grade()
 	{
-		if(Gpa=<10&&Gpa>9)
+		if(Gpa<=10&&Gpa>9)
 		{
-	      System.out.print("outstanding"+Gpa);		
+	      System.out.print("outstanding  A :"+Gpa);		
+		}
+		if(Gpa<=9&&Gpa>8)
+		{
+		      System.out.print("Excellent  B :"+Gpa);		
+		}
+		if(Gpa<=8&&Gpa>7)
+		{
+		      System.out.print("Good     C :"+Gpa);		
+		}
+		if(Gpa<=7&&Gpa>6)
+		{
+		      System.out.print("Average    D:"+Gpa);		
+
+		}
+		if(Gpa<=6&&Gpa>5) 
+		{
+		      System.out.print("pass   E  :"+Gpa);		
+
 		}
 	}
 	void display()
 	{
-		System.out.println("Student name"+name);
-		System.out.println("Student rollno"+roll_no);
-		System.out.println("Student marks1"+marks1);
-		System.out.println("Student marks2"+marks2);
-		System.out.println("Student marks3"+marks3);
-		System.out.println("Student marks4"+marks4);
-		System.out.println("Student marks5"+marks5);
-		System.out.println("Student total marks"+total);
-		System.out.println("Student averge marks"+avg);
-		System.out.println("Student percenteg marks"+per);
+		System.out.println("Student name:"+name);
+		System.out.println("Student rollno:"+roll_no);
+		System.out.println("Student marks1:"+marks1);
+		System.out.println("Student marks2:"+marks2);
+		System.out.println("Student marks3:"+marks3);
+		System.out.println("Student marks4:"+marks4);
+		System.out.println("Student marks5:"+marks5);
+		System.out.println("Student total marks:"+total);
+		System.out.println("Student averge marks:"+avg);
+		System.out.println("Student percenteag marks:"+per);
 		}
 	
 		public static void main(String[] args) {
-			University_Student s1=new  University_Student();
+			university_student s1=new  university_student();
 			s1.input();
 			s1.calculate();
 			s1.calculate_GPA();
