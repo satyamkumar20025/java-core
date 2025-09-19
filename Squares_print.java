74290780ce8674b39017b6e4a94e3a72ecea96aa
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-09-20 05:21:27 +0530

    Update and rename Vehicle_and_car.java to Squares_print.java

---
+++ Squares_print.java
@@ -0,0 +1,19 @@
+package For_Loop_Question;
+
+import java.util.Scanner;
+
+public class Squares {
+	public static void main(String[] args)
+	{
+		 Scanner sc = new Scanner(System.in);
+				System.out.print("you can find the squares of all nature number between 1 for n terms :");
+		         int n= sc.nextInt();
+		         int sum=0;
+		       for( int i=1;i<=n; i++)
+		       {
+		    	  
+		       
+				System.out.println("Enter the your nature number square:"+i*i);
+		       }
+}
+}
