1c9aaf36abffd1d5160cf7f2d15ed3ced905a273
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-09-20 05:30:33 +0530

    Update and rename person_Student_Inheriting.java to Multiplication_Table_print.java

---
+++ Multiplication_Table_print.java
@@ -0,0 +1,20 @@
+package For_Loop_Question;
+
+import java.util.Scanner;
+
+public class Multiplication {
+	public static void main(String[] args)
+	{
+		 Scanner sc = new Scanner(System.in);
+				System.out.print("you can find the Multiplication Table of all nature number between 1 for n terms :");
+		         int n= sc.nextInt();
+		         int sum=0;
+		       for( int i=1;i<=10; i++)
+		       {
+		    	  
+		       
+				System.out.println("Enter the your nature number Table:"+i*n);
+		       }
+}
+}
+
