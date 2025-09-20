ab77a660aec9c06114649edea8eed6897832d356
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-09-20 05:41:24 +0530

    Update and rename question6_while_loop.java to Reverseprint_loop.java

---
+++ Reverseprint_loop.java
@@ -0,0 +1,17 @@
+package For_Loop_Question;
+
+import java.util.Scanner;
+
+public class Reverse {
+
+	public static void main(String[] args)
+	{
+		 Scanner sc = new Scanner(System.in);
+				System.out.print("click between reverse number:");
+		         int n= sc.nextInt();
+		       for( int i=n;i>=1; i--)
+		       {
+		    	   System.out .println(i);
+		       }
+	}
+}
