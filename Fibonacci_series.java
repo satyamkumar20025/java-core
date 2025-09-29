19ce4d19e61e47fc44df95e134d45956ffa64d07
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-09-29 20:16:55 +0530

    Create Fibonacci_series.java

---
+++ Fibonacci_series.java
@@ -0,0 +1,24 @@
+package For_Loop_Question;
+
+import java.util.Scanner;
+
+public class Fibonacci_series {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter number of terms: ");
+	        int n = sc.nextInt();
+
+	        int first = 0, second = 1;
+
+	        System.out.print("Fibonacci Series: ");
+
+	        for (int i = 1; i <= n; i++) {
+	            System.out.print(first + " ");
+	            int next = first + second;
+	            first = second;
+	            second = next;
+	        }
+	    }
+	}
+
