d71cafd2f253f90f50a32b087fa469c3442fa4ed
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-09-30 12:30:22 +0530

    Create Left_Right_Shift.java

---
+++ Left_Right_Shift.java
@@ -0,0 +1,13 @@
+package OPERATOR_QUESTIONS;
+
+public class Left_Right_Shift {
+	public static void main(String[] args)
+	{
+		
+
+	int num = 4;
+	System.out.println("Left Shift: " + (num << 1));  // 4 * 2 = 8
+	System.out.println("Right Shift: " + (num >> 1)); // 4 / 2 = 2
+	}
+	
+}
