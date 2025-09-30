d1a162ea838d04cffd93060fe410b88bb3fd1da1
SatyamKumar20025 <satyamkumar6372@gmail.com>
2025-09-30 12:35:36 +0530

    Create max_ternary_operator.java

---
+++ max_ternary_operator.java
@@ -0,0 +1,12 @@
+
+package OPERATOR_QUESTIONS;
+
+public class max_ternary_operator {
+	public static void main(String[] args)
+	{
+		int x = 10, y = 20;
+		int max = (x > y) ? x : y;
+		System.out.println("Max: " + max);
+	}
+
+}
